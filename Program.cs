9616d7b84af8998e32d2c518d001df6b5043dda0
Dave <d.ruocco1987@googlemail.com>
2024-01-22 11:20:03 +0000

    Initial commit, sets up SQLite

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace HabitTracker
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=habit-Tracker.db";
+
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+                tableCmd.CommandText = "";
+                tableCmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
