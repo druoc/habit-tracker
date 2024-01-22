079db0ecacf8ea9a6070853c78b9145b929930e9
Dave <d.ruocco1987@googlemail.com>
2024-01-22 15:03:21 +0000

    Adds Get, Insert and Delete methods

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.Sqlite;
+using System.Globalization;
 using System.Runtime.CompilerServices;
 
 namespace HabitTracker
@@ -39,7 +40,7 @@ namespace HabitTracker
                 Console.WriteLine("Type 2 to insert record");
                 Console.WriteLine("Type 3 to delete record");
                 Console.WriteLine("Type 4 to update record");
-                Console.WriteLine("----------------------\n");
+                Console.WriteLine("-------------------------------\n");
 
                 string commandInput = Console.ReadLine();
 
@@ -49,25 +50,71 @@ namespace HabitTracker
                         Console.WriteLine("\nGoodbye!");
                         closeApp = true;
                         break;
-                    //case "1":
-                    //    GetAllRecords();
-                    //    break;
-                    //case "2":
-                    //    InsertRecord();
-                    //    break;
-                    //case "3":
-                    //    DeleteRecord();
-                    //    break;
-                    //case "4":
-                    //    UpdateRecord();
-                    //    break;
-                    //default:
-                    //    Console.WriteLine("\nInvalid command, please type a number from 0 to 4");
-                    //    break;
+                    case "1":
+                        GetAllRecords();
+                        break;
+                    case "2":
+                        InsertRecord();
+                        break;
+                    case "3":
+                        DeleteRecord();
+                        break;
+                        //case "4":
+                        //    UpdateRecord();
+                        //    break;
+                        //default:
+                        //    console.writeline("\ninvalid command, please type a number from 0 to 4");
+                        //    break;
                 }
             }
         }
-        private static void Insert()
+
+        private static void GetAllRecords()
+        {
+            Console.Clear();
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText =
+                    $"SELECT * FROM drinking_water";
+
+                List<DrinkingWater> tableData = new();
+
+                SqliteDataReader reader = tableCmd.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        tableData.Add(new DrinkingWater
+                        {
+                            Id = reader.GetInt32(0),
+                            Date = DateTime.ParseExact(reader.GetString(1), "dd-MM-yy", new CultureInfo("en-US")),
+                            Quantity = reader.GetInt32(2)
+                        }); 
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found");
+                }
+
+                connection.Close();
+                Console.WriteLine("||---------------------------||");
+                Console.WriteLine("      AVAILABLE RECORDS");
+                Console.WriteLine("||---------------------------||");
+
+                foreach (var drinkingWater in tableData)
+                {
+                    Console.WriteLine($"   {drinkingWater.Id} - {drinkingWater.Date.ToString("dd-MM-yy")} - Quantity: {drinkingWater.Quantity}");
+                }
+                Console.WriteLine("-------------------------------\n");
+            }
+
+        }
+        private static void InsertRecord()
         {
             string date = GetDateInput();
             int quantityOfWater = GetNumberInput("\n\nPlease input number of glasses or unit of your choice (no decimals\n\n");
@@ -86,7 +133,33 @@ namespace HabitTracker
 
         }
 
-        internal static string GetDateInput() 
+        private static void DeleteRecord()
+        {
+            Console.Clear();
+            GetAllRecords();
+
+            var recordId = GetNumberInput("\n\nPlease type the ID of the record you want to delete, or press 0 to return to the main menu");
+
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText =
+                    $"DELETE from drinking_water WHERE Id = '{recordId}'";
+
+                int rowCount = tableCmd.ExecuteNonQuery();
+
+                if (rowCount == 0)
+                {
+                    Console.WriteLine($"\n\nRecord with ID {recordId} does not exist \n\n");
+                    DeleteRecord();
+                }
+            }
+            Console.WriteLine($"\n\nRecord with ID {recordId} has been deleted.\n\n");
+        }
+
+        public static string GetDateInput() 
         {
             Console.WriteLine("\nPlease insert the date: (Format: dd-mm-yy) Type 0 to return to main menu");
             string dateInput = Console.ReadLine();
@@ -95,7 +168,7 @@ namespace HabitTracker
             return dateInput;
         }
 
-        internal static int GetNumberInput(string message)
+        public static int GetNumberInput(string message)
         {
             Console.WriteLine(message);
             string numberInput = Console.ReadLine();
@@ -104,6 +177,13 @@ namespace HabitTracker
 
             int finalInput = Convert.ToInt32(numberInput);
             return finalInput;
+        }
+
+        public class DrinkingWater
+        {
+            public int Id { get; set; }
+            public DateTime Date { get; set; }
+            public int Quantity { get; set; }
         }
     }
 }
